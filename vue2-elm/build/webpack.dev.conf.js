85a2450d04dd9ab8fe5a4a7bd9646d49ade3f1ca
1200 <1053182739@qq.com>
2018-04-25 21:06:13 +0800

    11
    11

--- vue2-elm/build/webpack.dev.conf.js
+++ vue2-elm/build/webpack.dev.conf.js
@@ -23,9 +23,9 @@ module.exports = merge(baseWebpackConfig, {
             'process.env': config.dev.env
         }),
         // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
         // https://github.com/ampedandwired/html-webpack-plugin
         new HtmlWebpackPlugin({
             filename: 'index.html',
